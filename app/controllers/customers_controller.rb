ae6cf4139b36a7c15c8e414a0aee256636b3d244
ManoelBertin <manoeltirj@gmail.com>
2024-07-30 23:49:50 -0300

    My Simple_form is running e ready

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,47 @@
 class CustomersController < ApplicationController
+  before_action :load_customer, only: %i[show edit update destroy]
+  def index
+    @customers = Customer.order(:name)
+
+  end
+
+  def show
+    
+  end
+
+  def new
+    @customer = Customer.new
+  end
+
+  def create
+    @customer = Customer.new customer_params
+
+    if @customer.save
+      redirect_to customer_path, status: :ok, notice: "Succeful Client Cadastred !!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:name, :dob, :mobile_phone, :email, :address)
+  end
+
+  def load_customer
+    @customers = Customer.find params(:id)
+  end
 end
